e36dbcdcb01c92abe3a0435ae107e689b50b4a74
DerekSunmo <145680189+DerekSunmo@users.noreply.github.com>
2023-09-22 01:07:18 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -379,7 +379,7 @@ function TodayCard({ cRef }) {
           } cursor-pointer today-card-cover absolute w-full h-full top-0`}
           style={{
             background:
-              "url('https://bu.dusays.com/2023/03/12/640dcd3a1b146.png') no-repeat center /cover"
+              "url('https://cdn.staticaly.com/gh/Sunhaha520/picx-images-hosting@master/20230922/tuo.2kyigld00rc0.webp') no-repeat center /cover"
           }}
         ></div>
       </div>
